e7534c2a64d4df886c718dfc4d18a9d911ab5d93
Ben Ubois <ben@benubois.com>
2015-10-09 13:20:14 -0700

    Moved to subdirectory.

--- .deploy/config/deploy.rb
+++ .deploy/config/deploy.rb
@@ -13,11 +13,8 @@ set :repo_url, "git@github.com:benubois/#{fetch(:application)}.git"
 namespace :deploy do
 
   after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, "cache:clear"
-      # end
+    on roles(:web) do
+
     end
   end
 
